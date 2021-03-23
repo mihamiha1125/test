3c1952a925d613cf8ca0e2f05ed442be45177217
mihamiha1125 <71978815+mihamiha1125@users.noreply.github.com>
2021-03-23 21:12:02 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,45 @@
+// $('.main1').slick({
+//   autoplay: true,
+//   autoplaySpeed: 5000,
+// });
+
+$(function () {
+  $('.wrap').hide().fadeIn(1500);
+});
+$('.btn2').on('click', function () {
+  $(window).scrollTop(0);
+});
+$('.btn3').on('click', function () {
+  $(window).scrollTop(750);
+});
+$('.btn4').on('click', function () {
+  $(window).scrollTop(1150);
+});
+$('.btn5').on('click', function () {
+  $(window).scrollTop(1800);
+});
+$('.btn6').on('click', function () {
+  $(window).scrollTop(2700);
+});
+$('.btn7').on('click', function () {
+  $(window).scrollTop(3600);
+});
+
+$('.btn2-2').on('click', function () {
+  $(window).scrollTop(0);
+});
+$('.btn3-2').on('click', function () {
+  $(window).scrollTop(370);
+});
+$('.btn4-2').on('click', function () {
+  $(window).scrollTop(740);
+});
+$('.btn5-2').on('click', function () {
+  $(window).scrollTop(990);
+});
+$('.btn6-2').on('click', function () {
+  $(window).scrollTop(1290);
+});
+$('.btn7-2').on('click', function () {
+  $(window).scrollTop(1340);
+});
